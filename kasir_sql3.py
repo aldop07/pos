a492997a2ab30045c9f9c08125710fe716d21818
Aldo <30534322+aldop07@users.noreply.github.com>
2023-01-14 10:59:53 +0700

    Update kasir_sql3.py

--- kasir_sql3.py
+++ kasir_sql3.py
@@ -5,7 +5,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import plotly.express as px
 from mlxtend.frequent_patterns import apriori, association_rules
-
+import fitz
+import requests
+from PIL import Image
 
 #   IRFAN NOVALDO HUANG
 
