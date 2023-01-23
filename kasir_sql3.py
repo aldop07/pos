7ce7a3812e6cb1204bb1331926cc9d875814cc7e
Aldo <30534322+aldop07@users.noreply.github.com>
2023-01-23 07:58:24 +0700

    Update kasir_sql3.py

--- kasir_sql3.py
+++ kasir_sql3.py
@@ -41,7 +41,7 @@ elif menu == 'Daftar Produk':
     df = pd.read_sql(query, cnx)
 
     # Tampilkan harga dalam bentuk angka dengan tanda titik sebagai pemisah ribuan
-    df['harga'] = df['harga'].apply(lambda x: '{:,}'.format(x))
+    df['harga'] = df['harga'].apply(lambda x: '{:,}'.format(x).replace(',', '.'))
 
     # Tampilkan stok dalam bentuk angka tanpa desimal
     df['stok'] = df['stok'].apply(lambda x: int(x) if x == x else x)
@@ -64,6 +64,8 @@ elif menu == 'Daftar Produk':
             cursor.execute(query_update, (stok_baru, produk))
             cnx.commit()
             st.success('Stok produk berhasil diubah')
+    else:
+        st.warning('Input dengan teliti')
 
 # Tampilan menu Tambah Produk
 elif menu == 'Tambah Produk':
@@ -73,11 +75,14 @@ elif menu == 'Tambah Produk':
     harga_pokok = st.number_input('Harga Pokok',0)
     stok_produk = st.number_input('Stok Produk',0)
     if st.button('Simpan'):
-        cursor = cnx.cursor()
-        query = 'INSERT INTO produk (nama, harga, stok, harga_pokok) VALUES (?, ?, ?, ?)'
-        cursor.execute(query, (nama_produk, harga_produk, stok_produk, harga_pokok))
-        cnx.commit()
-        st.success('Produk berhasil disimpan')
+        if nama_produk == "" or harga_pokok < 1000 or harga_produk < 1000 :
+            st.warning('Input dengan benar')
+        else:
+            cursor = cnx.cursor()
+            query = 'INSERT INTO produk (nama, harga, stok, harga_pokok) VALUES (?, ?, ?, ?)'
+            cursor.execute(query, (nama_produk, harga_produk, stok_produk, harga_pokok))
+            cnx.commit()
+            st.success('Produk berhasil disimpan')
 
 # Tampilan menu Tambah Transaksi
 elif menu == 'Tambah Transaksi':
@@ -94,79 +99,87 @@ elif menu == 'Tambah Transaksi':
     jumlah_produk = st.number_input('Jumlah Produk',0)
     if jumlah_produk == 0:
         st.error('Jumlah produk tidak boleh 0')
-        
-    elif st.button('Simpan'):
-    # Buat objek cursor
-        cursor = cnx.cursor()
-        query = 'SELECT harga, stok FROM produk WHERE nama = ?'
-        cursor.execute(query, (nama_produk,))
-        result = cursor.fetchone()
-        harga_produk = result[0]
-        stok_produk = result[1]
-        total_harga = harga_produk * jumlah_produk
-        if stok_produk >= jumlah_produk:
+    else:   
+        if st.button('Simpan'):
+        # Buat objek cursor
+            cursor = cnx.cursor()
+            query = 'SELECT harga, stok FROM produk WHERE nama = ?'
+            cursor.execute(query, (nama_produk,))
+            result = cursor.fetchone()
+            harga_produk = result[0]
+            stok_produk = result[1]
+            total_harga = harga_produk * jumlah_produk
+            if stok_produk >= jumlah_produk:
+                
+                # Tambahkan transaksi baru ke tabel transaksi
+                query = 'INSERT INTO transaksi (tanggal, nama_pelanggan, nama, jumlah, harga, total) VALUES (?, ?, ?, ?, ?, ?)'
+                cursor.execute(query, (tanggal ,nama_pelanggan, nama_produk, jumlah_produk, harga_produk, total_harga))
             
-            # Tambahkan transaksi baru ke tabel transaksi
-            query = 'INSERT INTO transaksi (tanggal, nama_pelanggan, nama, jumlah, harga, total) VALUES (?, ?, ?, ?, ?, ?)'
-            cursor.execute(query, (tanggal ,nama_pelanggan, nama_produk, jumlah_produk, harga_produk, total_harga))
-           
-            # Kurangi stok produk yang dibeli
-            query = 'UPDATE produk SET stok = stok - ? WHERE nama = ?'
-            cursor.execute(query, (jumlah_produk, nama_produk))
-            cnx.commit()
-            st.success('Transaksi berhasil disimpan')
-        else:
-            st.error('Stok produk tidak mencukupi')
+                # Kurangi stok produk yang dibeli
+                query = 'UPDATE produk SET stok = stok - ? WHERE nama = ?'
+                cursor.execute(query, (jumlah_produk, nama_produk))
+                cnx.commit()
+                st.success('Transaksi berhasil disimpan')
+            else:
+                st.error('Stok produk tidak mencukupi')
 
+# Tampilan menu Tambah Pengeluaran
 # Tampilan menu Tambah Pengeluaran
 elif menu == 'Tambah Pengeluaran':
     st.header('Tambah Pengeluaran')
     tanggal = st.date_input('Tanggal')
-    nama_pengeluaran = st.text_input('Jenis Pengeluaran')
+    ket_pengeluaran = st.text_input('Keterangan Pengeluaran')
     jumlah_pengeluaran = st.number_input('Jumlah Pengeluaran',0)
     if st.button('Simpan'):
-        cursor = cnx.cursor()
-        query = 'INSERT INTO pengeluaran (tanggal, nama_pengeluaran, jumlah_pengeluaran) VALUES (?, ?, ?)'
-        cursor.execute(query, (tanggal, nama_pengeluaran, jumlah_pengeluaran))
-        cnx.commit()
-        st.success('Pengeluaran berhasil disimpan')
+        if ket_pengeluaran == "":
+            st.warning("Sebutkan Keterangan Pengeluaran")
+        else:
+            cursor = cnx.cursor()
+            query = 'INSERT INTO pengeluaran (tanggal, nama_pengeluaran, jumlah_pengeluaran) VALUES (?, ?, ?)'
+            cursor.execute(query, (tanggal, ket_pengeluaran, jumlah_pengeluaran))
+            cnx.commit()
+            st.success('Pengeluaran berhasil disimpan')
+        
 
 # Tampilan menu Laba
 elif menu == 'Laba':
     st.header('Laba')
     tanggal_awal = st.date_input('Tanggal Awal')
     tanggal_akhir = st.date_input('Tanggal Akhir')
-    
-    # Hitung total pengeluaran
-    query = 'SELECT SUM(jumlah_pengeluaran) FROM pengeluaran WHERE tanggal BETWEEN ? AND ?'
-    cursor = cnx.cursor()
-    cursor.execute(query, (tanggal_awal, tanggal_akhir))
-    result = cursor.fetchone()
-    total_pengeluaran = result[0]
-    total_pengeluaran_rupiah = 'Rp. {:,}'.format(total_pengeluaran)
-    st.write('Total Pengeluaran:', total_pengeluaran_rupiah)
+    if st.button('CEK LABA'):
 
-    # Hitung total transaksi
-    query = 'SELECT SUM(total) FROM transaksi WHERE tanggal BETWEEN ? AND ?'
-    cursor = cnx.cursor()
-    cursor.execute(query, (tanggal_awal, tanggal_akhir))
-    result = cursor.fetchone()
-    total_transaksi = result[0]
-    total_transaksi_rupiah = 'Rp. {:,}'.format(total_transaksi)
-    st.write('Total transaksi:', total_transaksi_rupiah)
-    
-    # Hitung pemasukan
-    query = 'SELECT SUM(total - harga_pokok * jumlah) FROM transaksi JOIN produk ON transaksi.nama = produk.nama WHERE transaksi.tanggal BETWEEN ? AND ?'
-    cursor.execute(query, (tanggal_awal, tanggal_akhir))
-    result = cursor.fetchone()
-    pemasukan = result[0]
-    pemasukan_rupiah = 'Rp. {:,}'.format(pemasukan)
-    st.write('Keuntungan:', pemasukan_rupiah)
-    
-    # Hitung laba
-    laba = pemasukan - total_pengeluaran
-    laba_rupiah = 'Rp. {:,}'.format(laba)
-    st.write('Laba Bersih:', laba_rupiah)
+    # Hitung total pengeluaran
+        query = 'SELECT SUM(jumlah_pengeluaran) FROM pengeluaran WHERE tanggal BETWEEN ? AND ?'
+        cursor = cnx.cursor()
+        cursor.execute(query, (tanggal_awal, tanggal_akhir))
+        result = cursor.fetchone()
+        total_pengeluaran = result[0]
+        total_pengeluaran_rupiah = 'Rp. {:,}'.format(total_pengeluaran).replace(',', '.')
+        st.write('Total Pengeluaran:', total_pengeluaran_rupiah)
+
+        # Hitung total transaksi
+        query = 'SELECT SUM(total) FROM transaksi WHERE tanggal BETWEEN ? AND ?'
+        cursor = cnx.cursor()
+        cursor.execute(query, (tanggal_awal, tanggal_akhir))
+        result = cursor.fetchone()
+        total_transaksi = result[0]
+        total_transaksi_rupiah = 'Rp. {:,}'.format(total_transaksi).replace(',', '.')
+        st.write('Total transaksi:', total_transaksi_rupiah)
+        
+        # Hitung pemasukan
+        query = 'SELECT SUM(total - harga_pokok * jumlah) FROM transaksi JOIN produk ON transaksi.nama = produk.nama WHERE transaksi.tanggal BETWEEN ? AND ?'
+        cursor.execute(query, (tanggal_awal, tanggal_akhir))
+        result = cursor.fetchone()
+        pemasukan = result[0]
+        pemasukan_rupiah = 'Rp. {:,}'.format(pemasukan).replace(',', '.')
+        st.write('Keuntungan:', pemasukan_rupiah)
+        
+        # Hitung laba
+        laba = pemasukan - total_pengeluaran
+        laba_rupiah = 'Rp. {:,}'.format(laba).replace(',', '.')
+        st.write('Laba Bersih:', laba_rupiah)
+    else:
+        st.warning('LABA HANYA DAPAT DIHITUNG JIKA ADA PENGELUARAN')
 
 # Tampilan menu Riwayat Transaksi
 elif menu == 'Riwayat Transaksi':
@@ -176,32 +189,28 @@ elif menu == 'Riwayat Transaksi':
     df = df.sort_values(by='tanggal', ascending=False)
     
     # Tampilkan harga dan total dalam bentuk angka dengan tanda titik sebagai pemisah ribuan
-    df['harga'] = df['harga'].apply(lambda x: '{:,}'.format(x))
-    df['total'] = df['total'].apply(lambda x: '{:,}'.format(x))
+    df['harga'] = df['harga'].apply(lambda x: '{:,}'.format(x).replace(',', '.'))
+    df['total'] = df['total'].apply(lambda x: '{:,}'.format(x).replace(',', '.'))
     st.dataframe(df)
 
 
     # Buat grafik jumlah penjualan per bulan
     tanggal_mulai = st.date_input('Tanggal Mulai')
     tanggal_akhir = st.date_input('Tanggal Akhir')
-    if tanggal_mulai and tanggal_akhir:
+    if st.button('CEK GRAFIK'):
         query = "SELECT date(tanggal) as tanggal, SUM(jumlah) as jumlah_penjualan FROM transaksi WHERE tanggal BETWEEN ? AND ? GROUP BY date(tanggal)"
         df = pd.read_sql(query, cnx, params=(tanggal_mulai, tanggal_akhir))
         fig = px.bar(df, x='tanggal', y='jumlah_penjualan')
         fig.update_layout(autosize=True)
-        fig.update_layout(width=800, height=400)
         st.plotly_chart(fig)
-
-
-    if tanggal_mulai and tanggal_akhir:
+        st.warning('Input dengan teliti')
         query = "SELECT nama, tanggal, SUM(jumlah) as jumlah_penjualan FROM transaksi WHERE tanggal BETWEEN ? AND ? GROUP BY nama, date(tanggal)"
         df = pd.read_sql(query, cnx, params=(tanggal_mulai, tanggal_akhir))
         df = df.pivot(index='tanggal', columns='nama', values='jumlah_penjualan').fillna(0)
-        st.line_chart(df, width=800, height=400)
-
-
+        st.line_chart(df)
+    else:
+        st.warning('Input dengan teliti')
             
-# Tampilan menu Data Mining
 elif menu == 'Data Mining':
     st.header('Data Mining')
 
@@ -213,71 +222,82 @@ elif menu == 'Data Mining':
         # Input tanggal awal dan akhir
         tanggal_mulai = st.date_input("Tanggal Mulai")
         tanggal_akhir = st.date_input("Tanggal Akhir")
+        # Mendefinisikan nilai minimum support dan minimum confidence
+        minimum_support = st.number_input("Nilai minimum support:",0.01)
+        minimum_confidence = st.number_input("Nilai minimum confidence:",0.01)
         
-        if tanggal_mulai and tanggal_akhir:
+        #if tanggal_mulai and tanggal_akhir:
             # Membaca data transaksi dari database SQLite
-            query = 'SELECT tanggal, nama_pelanggan, nama FROM transaksi WHERE tanggal BETWEEN ? AND ?'
-            df = pd.read_sql(query, cnx, params=(tanggal_mulai, tanggal_akhir))
+        query = 'SELECT tanggal, nama_pelanggan, nama FROM transaksi WHERE tanggal BETWEEN ? AND ?'
+        df = pd.read_sql(query, cnx, params=(tanggal_mulai, tanggal_akhir))
 
             # Mengubah tanggal yang ditampilkan  dataframe menjadi objek datatime
-            df['tanggal'] = pd.to_datetime(df['tanggal'])
+        df['tanggal'] = pd.to_datetime(df['tanggal'])
 
             # Memfilter data transaksi berdasarkan tanggal mulai dan akhir
-            df = df[(df['tanggal'] >= pd.to_datetime(tanggal_mulai)) & (df['tanggal'] <= pd.to_datetime(tanggal_akhir))]
+        df = df[(df['tanggal'] >= pd.to_datetime(tanggal_mulai)) & (df['tanggal'] <= pd.to_datetime(tanggal_akhir))]
             
             # Mengubah data menjadi tabulasi
-            df['tanggal_nama_pelanggan'] = df['tanggal'].astype(str) +'-'+ df['nama_pelanggan']
-            tabular = pd.crosstab(df['tanggal_nama_pelanggan'],df['nama'])
+        df['tanggal_nama_pelanggan'] = df['tanggal'].astype(str) +'-'+ df['nama_pelanggan']
+        tabular = pd.crosstab(df['tanggal_nama_pelanggan'],df['nama'])
 
+
+            #dta = pd.DataFrame(tabular)
+            #download = dta.to_excel
+            #if download :
+            #        with open("Tabulasi.xlsx", "wb") as f: # buka file Tabulasi.xls dalam mode binary write
+
+             #           dta.to_excel(f) # menulis dataframe dta ke file excel
+
+              #      with open("Tabulasi.xlsx", "rb") as f: #buka file Tabulasi.xls dalam mode binary read
+
+               #         excel_file = f.read() #membaca data biner
+
+                #    st.download_button(label="Download Excel", data=excel_file, file_name="Tabulasi.xlsx", mime='text/xlsx')
+            #else:
+            #    st.warning('Input dengan teliti')
             # Encoding data
-            def hot_encode(x) :
-                if (x<=0):
-                    return 0
-                if (x>=1):
-                    return 1
-
-            # Mengubah data menjadi binominal
-            tabular_encode = tabular.applymap(hot_encode)
-
-            # Mendefinisikan nilai minimum support dan minimum confidence
-            minimum_support = st.number_input("Nilai minimum support:",0.01)
-            if minimum_support <= 0:
-                st.warning("Nilai minimum support tidak boleh kosong atau nol.")
-            minimum_confidence = st.number_input("Nilai minimum confidence:",0.01)
-            if minimum_confidence <= 0:
-                st.warning("Nilai minimum confidence tidak boleh kosong atau nol.")
-
-            # Membuat model Apriori
-            frq_items = apriori(tabular_encode, min_support=minimum_support, use_colnames= True)
-
-            # Mengumpulkan aturan dalam dataframe
-            rules = association_rules(frq_items, metric="lift",min_threshold=minimum_confidence)
-            rules = rules.sort_values(['confidence','lift'], ascending=[False, False])
+        def hot_encode(x) :
+            if (x<=0):
+                return 0
+            if (x>=1):
+                return 1
 
             # Menampilkan hasil dari algoritma Apriori
-            if st.button("PROSES"):
+        if st.button("PROSES"):
                 st.success('HASIL PERHITUNGAN APRIORI')
+            # Mengubah data menjadi binominal
+                tabular_encode = tabular.applymap(hot_encode)
 
+                
+                # Membuat model Apriori
+                frq_items = apriori(tabular_encode, min_support=minimum_support, use_colnames= True)
+
+                # Mengumpulkan aturan dalam dataframe
+                rules = association_rules(frq_items, metric="lift",min_threshold=minimum_confidence)
+                rules = rules.sort_values(['confidence','lift'], ascending=[False, False])
                 # Mengubah nilai support, confidence, dan lift menjadi persentase
                 rules[["antecedent support","consequent support","support","confidence"]] = rules[["antecedent support","consequent support","support","confidence"]].applymap(lambda x: "{:.2f}%".format(x*100))
 
                 # Menampilkan hasil algoritma apriori dalam bentuk dataframe
                 st.dataframe(rules.applymap(lambda x: ','.join(x) if type(x) == frozenset else x))
+        else:
+                st.warning('Input dengan teliti')
 
     elif sub_menu == 'Forecasting':
         st.header('Forecasting')
-        st.warning('BELUM FIX')
-        average = st.number_input('Masukan Jumlah Rentang',0)
+        st.info('BELUM FIX')
+        average = st.number_input('Masukan Jumlah Rentang',min_value=1)
         query = "SELECT nama FROM produk"
         df = pd.read_sql(query, cnx)
         nama_item = st.selectbox("Pilih produk ", df['nama'].tolist())
-        query = "SELECT tanggal, jumlah FROM transaksi WHERE nama = ?"
-        df = pd.read_sql(query, cnx,params=(nama_item,))
-        df.set_index('tanggal', inplace=True)
-        df = df.groupby(['tanggal'])['jumlah'].sum().reset_index()
-        df['moving_avg'] = df['jumlah'].shift(1).rolling(window=average).mean()
-        df = df.sort_values(by='tanggal', ascending=False)
-        st.dataframe(df)
-else:
-    st.error('Invalid menu selected')
-    st.info('Please select a valid option')
+        if st.button('CEK FORECASTING'):
+            query = "SELECT tanggal, jumlah FROM transaksi WHERE nama = ?"
+            df = pd.read_sql(query, cnx,params=(nama_item,))
+            df.set_index('tanggal', inplace=True)
+            df = df.groupby(['tanggal'])['jumlah'].sum().reset_index()
+            df['moving_avg'] = df['jumlah'].shift(1).rolling(window=average).mean()
+            df = df.sort_values(by='tanggal', ascending=False)
+            st.dataframe(df)
+        else:
+            st.warning('Input dengan teliti')
