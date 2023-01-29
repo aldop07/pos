c3380f84c007274af67609b1565e67c46d3e9c00
Aldo <30534322+aldop07@users.noreply.github.com>
2023-01-30 06:11:08 +0700

    Update kasirmysql.py

--- kasirmysql.py
+++ kasirmysql.py
@@ -131,11 +131,14 @@ elif menu == 'Daftar Produk':
        produk = st.selectbox("Id Produk", df['id'].tolist())
        jumlah = st.number_input("Jumlah", value=df.loc[df['id'] == produk, 'jumlah_update'].values[0])
        if st.button('Edit'):
-            cursor = cnx.cursor()
-            query = "UPDATE update_produk SET jumlah_update = %s WHERE id = %s"
-            cursor.execute(query, (jumlah, produk))
-            cnx.commit()
-            st.success('Produk berhasil diedit')
+           if jumlah < 1:
+                st.error('Jumlah yang diedit tidak bisa kuran dari 1')
+           else:
+                cursor = cnx.cursor()
+                query = "UPDATE update_produk SET jumlah_update = %s WHERE id = %s"
+                cursor.execute(query, (jumlah, produk))
+                cnx.commit()
+                st.success('Produk berhasil diedit')
             
        if st.button('Hapus'):
             query = 'DELETE FROM update_produk WHERE id = %s'
