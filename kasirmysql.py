a9b29c6126b8a06cc7e579c0a4ec97ec098924a2
Aldo <30534322+aldop07@users.noreply.github.com>
2023-01-29 22:03:22 +0700

    Update kasirmysql.py

--- kasirmysql.py
+++ kasirmysql.py
@@ -602,5 +602,3 @@ elif menu == 'Data Mining':
             df['moving_avg'] = df['jumlah'].shift(1).rolling(window=average).mean()
             df = df.sort_values(by='tanggal', ascending=False)
             st.dataframe(df)
-cursor.close()
-cnx.close()
