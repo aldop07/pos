e365dd6647108df1419d2a58a3a04126559ca84c
Aldo <30534322+aldop07@users.noreply.github.com>
2023-01-29 21:29:30 +0700

    Update edit_kas.py

--- edit_kas.py
+++ edit_kas.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import mysql.connector
 import pandas as pd
+import numpy as np
 
 
 # Koneksi ke database MySQL
@@ -17,14 +18,20 @@ df = df.sort_values(by='id', ascending=False)
 st.dataframe(df)
 id = st.selectbox("Id Produk", df['id'].tolist())
 jumlah_kas = st.number_input("Kas",value=df.loc[df['id'] == id, 'kas_awal'].values[0])
+
 if st.button('Hapus'):
     query = 'DELETE FROM kas WHERE id = %s'
     cursor = cnx.cursor()
     cursor.execute(query, (id,))
     cnx.commit()
     st.success('Produk berhasil dihapus')
-if st.button('edit'):
+    
+if st.button('Edit'):
     cursor = cnx.cursor()
     query = "UPDATE id SET id = %s, kas_awal WHERE id = %s"
     cursor.execute(query, (id, jumlah_kas))
     cnx.commit()
+    st.success('Data berhasil diperbarui')
+    
+cursor.close()
+cnx.close()
