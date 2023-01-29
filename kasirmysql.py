b53b044c1ffa89b0d54d5777277ab45eb95b470d
Aldo <30534322+aldop07@users.noreply.github.com>
2023-01-29 20:32:10 +0700

    Update kasirmysql.py

--- kasirmysql.py
+++ kasirmysql.py
@@ -433,7 +433,7 @@ elif menu == 'Laba':
             if kas_awal is None:
                 kas_awal = 0
                 
-            kas = kas_awal + total_transaksi_semua - (total_belanja_semua + total_pengeluaran_semua)
+            kas = total_transaksi_semua - ((kas_awal - total_belanja_semua) + total_pengeluaran_semua)
             kas = 'Rp. {:,}'.format(kas).replace(',', '.')
             
             if total_pengeluaran == 0 or total_transaksi == 0 or pemasukan == 0 or laba == 0 or modal_now == 0:
