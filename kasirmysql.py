7463f70be988b1b8bbc7c9cf32c21156a6a5d1d9
Aldo <30534322+aldop07@users.noreply.github.com>
2023-01-30 15:43:21 +0700

    Update kasirmysql.py

--- kasirmysql.py
+++ kasirmysql.py
@@ -604,4 +604,9 @@ elif menu == 'Data Mining':
         if st.button('CEK FORECASTING'):
             query = "SELECT tanggal, jumlah FROM transaksi WHERE nama = %s"
             df = pd.read_sql(query, cnx,params=(nama_item,))
+            df.set_index('tanggal', inplace=True)
+            df = df.groupby(['tanggal'])['jumlah'].sum().reset_index()
+            df['moving_avg'] = df['jumlah'].shift(1).rolling(window=average).mean()
+            df['forecast'] = df['moving_avg'].shift(1).rolling(window=average).mean()
+            df = df.fillna(0)
             st.dataframe(df)
