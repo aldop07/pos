b8c0c4180cded73b7294a8fa59c9d30ac787144d
Aldo <30534322+aldop07@users.noreply.github.com>
2023-01-30 17:01:51 +0700

    Update kasirmysql.py

--- kasirmysql.py
+++ kasirmysql.py
@@ -608,10 +608,11 @@ elif menu == 'Data Mining':
             df.set_index('tanggal', inplace=True)
             df = df.groupby(['tanggal'])['jumlah'].sum().reset_index()
             df['moving_avg'] = df['jumlah'].shift(1).rolling(window=average).mean()
+            df = df.fillna(0)
             last_date = df['tanggal'].max()
             new_date_range = pd.date_range(last_date + pd.Timedelta(1, unit='D'), periods=tambah_baris, freq='D')
-            new_df = pd.DataFrame({'tanggal': new_date_range, 'jumlah': ['moving_avg']*tambah_baris, 'moving_avg': ['moving_avg']*tambah_baris})
+            new_df = pd.DataFrame({'tanggal': new_date_range, 'jumlah': [0]*tambah_baris, 'moving_avg': [0]*tambah_baris})
             df = pd.concat([df, new_df])
             df['moving_avg'] = df['jumlah'].shift(1).rolling(window=average).mean()
-            df = df.fillna(0)
+            df.loc[df['tanggal'].isin(new_date_range), 'jumlah'] = df.loc[df['tanggal'].isin(new_date_range), 'moving_avg']
             st.dataframe(df)
