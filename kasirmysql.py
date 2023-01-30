850c17717216dceacaa507d435076c0011820f18
Aldo <30534322+aldop07@users.noreply.github.com>
2023-01-31 05:31:36 +0700

    Update kasirmysql.py

--- kasirmysql.py
+++ kasirmysql.py
@@ -617,7 +617,7 @@ elif menu == 'Data Mining':
                 new_row = pd.DataFrame({
                     'tanggal': [next_date],
                     'moving_avg': [df['jumlah'].iloc[-average:].mean()],
-                    'jumlah': [df['moving_avg'].iloc[]]
+                    'jumlah': [df['moving_avg']]
                 })
                 df = pd.concat([df, new_row])
             st.dataframe(df)
