26f779dbc2818de6399073997020085e18d1f97f
Aldo <30534322+aldop07@users.noreply.github.com>
2023-01-30 16:41:53 +0700

    Update kasirmysql.py

--- kasirmysql.py
+++ kasirmysql.py
@@ -609,7 +609,7 @@ elif menu == 'Data Mining':
             df = df.groupby(['tanggal'])['jumlah'].sum().reset_index()
             df['moving_avg'] = df['jumlah'].shift(1).rolling(window=average).mean()
             df = df.fillna(0)
-            last_date = df['tanggal'].max() + 1
+            last_date = df['tanggal'].max()
             new_date_range = pd.date_range(last_date, periods=tambah_baris, freq='D')
             new_df = pd.DataFrame({'tanggal': new_date_range, 'jumlah': [0]*tambah_baris, 'moving_avg': [0]*tambah_baris})
             df = pd.concat([df, new_df])
