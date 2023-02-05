10491741ef80b22844045c493fbea3ab52c057b9
Aldo <30534322+aldop07@users.noreply.github.com>
2023-02-05 20:16:49 +0700

    Update kasir_kalimantan.py

--- kasir_kalimantan.py
+++ kasir_kalimantan.py
@@ -9,7 +9,7 @@ from mlxtend.frequent_patterns import apriori, association_rules
 #   IRFAN NOVALDO HUANG
 
 # Koneksi ke database SQLite
-cnx = sqlite3.connect('kasir.db')
+cnx = sqlite3.connect('kasir_kalimantan.db')
 
 # Buat titit
 icon = 'https://e7.pngegg.com/pngimages/263/96/png-clipart-hijab-islam-islamic-background-brown-food-thumbnail.png'
